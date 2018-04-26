86d84a67c42cef82bb743f5bf3abfbd85d230f07
james20902 <james20902@gmail.com>
2018-04-25 21:42:20 -0400

    added documentation, as well as removed the function in which we grab the 2 biggest contours

--- Vision2018.py
+++ Vision2018.py
@@ -1,94 +1,48 @@
+#import everything we need
 import socket
 import time
 import cv2
 import numpy as np
 import os
 
-#os.system("uvcdynctrl -s 'Exposure, Auto' 1")
-#os.system("uvcdynctrl -s 'Exposure (Absolute)' 5")
-
+#grab log data for debug
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
+#define our UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 UDP_IP = "10.51.15.2"
 UDP_PORT = 5800
 
-
-gearcam = cv2.VideoCapture(1)
-gearcam.set(3, 160)
-gearcam.set(4, 120)
-
+#assign opencv camera to camera0
 boilercam = cv2.VideoCapture(0)
+#set propid(3, meaning camera width) to 160
 boilercam.set(3, 160)
+#set propid(4, meaing camera height) to 120
 boilercam.set(4, 120)
 
 counter = 0
 
-def getOffsetsGear(default):
-	ret, frame = gearcam.read()
-	height, width, channels = frame.shape
-	hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
-	lower = np.array([20, 150, 150])
-	upper = np.array([75, 255, 255])
-	thresh = cv2.inRange(hsv, lower, upper)
-
-	#cv2.imshow('unfiltered', frame)
-	#cv2.imshow('filtered', thresh)
-	cv2.waitKey(1)
-
-	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-	maxarea = 0
-	almostmaxarea = 0
-	centerx1 = 0
-	centerx2 = 0
-	centerx = 0
-	centery1 = 0
-	centery2 = 0
-	centery = 0
-	contour1 = 0
-	
-	pixelOffsetX = 0
-	pixelOffsetY = 0
-	
-	for i in range(len(contours)):
-		c = contours[i]
-		m = cv2.moments(c)
-		if m['m00'] > maxarea:
-			centerx1 = m['m10'] / m['m00']
-			centery1 = m['m01'] / m['m00']
-			maxarea = m['m00']
-			contourIndex = i
-	
-	for i in range(len(contours)):
-		c = contours[i]
-		m2 = cv2.moments(c)
-		if m2['m00'] > almostmaxarea and i != contourIndex:
-			centerx2 = m2['m10'] / m2['m00']
-			centery2 = m2['m01'] / m2['m00']
-			almostmaxarea = m2['m00']
-	
-	centerx = (centerx1 + centerx2) / 2
-		
-	pixelOffsetX = centerx - (width / 2)
-
-	print str(maxarea) + ' ' + str(almostmaxarea) + ' ' + str(pixelOffsetX)
-
-	if maxarea > 40 and almostmaxarea > 40:
-		return pixelOffsetX
-	else:
-		return default
-	
+#finding the Boiler 
 def getOffsetsBoiler():
+	#grab frame
 	ret, frame = boilercam.read()
+	#extract frame data
 	height, width, channels = frame.shape
+	#convert our color data from standard RGB to HSV
+	#HSV stands for hue saturation value
+	#see here https://en.wikipedia.org/wiki/HSL_and_HSV
 	hsv = cv2.cvtColor(frame, cv2.COLOR_RGB2HSV)
+	#specify the lowest and highest colors we could possibly want
 	lower = np.array([35, 100, 150])
 	upper = np.array([75, 255, 220])
+	#take out any frame data that IS NOT within our defined range
 	thresh = cv2.inRange(hsv, lower, upper)
+	#create contours, see here 
+	#http://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_contours/py_contours_begin/py_contours_begin.html#contours-getting-started
 	contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+	#initialize some variables for later
 	maxarea = 0
 	centerx = 0
 	centery = 0
@@ -96,15 +50,17 @@ def getOffsetsBoiler():
 	
 	pixelOffsetX = 0
 	pixelOffsetY = 0
-	
+	#within our list of contours
 	for c in contours:
 		m = cv2.moments(c)
+		#keep going down the list and find the biggest possible one.
+		#once the biggest one is found, assume that is our target
 		if m['m00'] > maxarea:
 			centerx = m['m10'] / m['m00']
 			centery = m['m01'] / m['m00']
 			maxarea = m['m00']
 			contour = c
-		
+	#camera offset to account for any perspective differences	
 	pixelOffsetX = centerx - (width / 2)
 	pixelOffsetY = -(centery - (height / 2))
 
@@ -114,9 +70,11 @@ def getOffsetsBoiler():
 	return pixelOffsetX, pixelOffsetY
 
 lastTime = time.clock()
+
+#great, now that we have our data that we got from the frame we captured...
 while True:
+	#grab our x y data from the boiler function
 	x, y = getOffsetsBoiler()
-	gear = getOffsetsGear(1000)
 
 	#print str(counter)
 	counter += 1
@@ -124,9 +82,8 @@ while True:
 	#print str(x)
 	#print str(y)
 	#print str(gear)
-	sock.sendto(str ("x " + x), (UDP_IP, UDP_PORT))
-	sock.sendto(str ("y " + y), (UDP_IP, UDP_PORT))
-	sock.sendto(str ("g " + gear), (UDP_IP, UDP_PORT))
+	sock.sendto(("x " + x), (UDP_IP, UDP_PORT))
+	sock.sendto(("y " + y), (UDP_IP, UDP_PORT))
 
 	nowTime = time.clock()
 	#print 1 / (nowTime - lastTime)
